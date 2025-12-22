cbf5111968f14737aba97cfa5cf6cc56a4123884
Mikhail Solovyov <solovev02@yandex.ru>
2025-12-22 18:48:09 +0300

    added some properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,6 +14,7 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
+
     @BeforeAll
     static void beforeAll() {
         browser = BrowserstackDriver.class.getName();
