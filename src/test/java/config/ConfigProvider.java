89b17d00321f138b0c3715a1766dae22610aeae0
Mikhail Solovyov <solovev02@yandex.ru>
2025-12-29 21:46:53 +0300

    changed ConfigProvider

--- src/test/java/config/ConfigProvider.java
+++ src/test/java/config/ConfigProvider.java
@@ -4,13 +4,9 @@ import org.aeonbits.owner.ConfigFactory;
 
 public class ConfigProvider {
 
-    private static final MobileConfig CONFIG = ConfigFactory.create(
-            MobileConfig.class, System.getProperties()
-    );
-
     private ConfigProvider() {}
 
     public static MobileConfig getConfig() {
-        return CONFIG;
+        return ConfigFactory.create(MobileConfig.class);
     }
 }
