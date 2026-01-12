381e414a64cb7a3b17eebe0e09303f17e0dcc245
Mikhail Solovyov <solovev02@yandex.ru>
2026-01-12 20:14:34 +0300

    --

--- src/test/java/tests/WikiTests.java
+++ src/test/java/tests/WikiTests.java
@@ -16,7 +16,6 @@ public class WikiTests extends TestBase {
 
     @Test
     void successfulSearchTest() {
-        back();
         step("Type search", () -> {
             $(accessibilityId("Search Wikipedia")).click();
             $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
@@ -27,7 +26,6 @@ public class WikiTests extends TestBase {
 
     @Test
     void successfulOpenSearchTest() {
-        back();
         step("Open search", () -> {
             $(accessibilityId("Search Wikipedia")).click();
         });
